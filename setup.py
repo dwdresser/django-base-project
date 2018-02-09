24b6bee9edee0c233de57ce6a9fa61b067920d44
Doug Dresser <dwdresser@wisc.edu>
2018-02-09 11:18:50 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='django_project',
+      version='0.1',
+      description='Base Django template for starting projects.',
+      url='http://github.com/dwdresser/django_project',
+      author='Doug Dresser',
+      author_email='dwdresser@wisc.edu',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[],
+     )
